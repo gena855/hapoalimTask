86c9987964c1f17276845284bb9d3548e835c0e6
genadi <gena855@gmail.com>
2021-11-14 16:37:35 +0200

    update

--- flask-Dockerfile/app.py
+++ flask-Dockerfile/app.py
@@ -1,6 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+import os
+myvar = os.environ.get('MYVAR')
+print(myvar)
+
+a_file = open("/var/secrets/FLASK_SECRET")
+file_contents = a_file.read()
+print(file_contents)
+
 @app.route('/')
 def hello_world():
     return 'Hello, Flask!'
